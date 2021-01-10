49f53ddd3fecf6e9ff5f92c917c933b3fd959004
carolinaferreira <carol.mf14@gmail.com>
2021-01-10 14:28:53 -0300

    Attempt to fix deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  namespace 'api' do
-  	namespace 'v1' do
-		resources :pokemons, only: :index
-		resources :players, only: [:index, :create]
-		resources :trades, only: [:index, :create]
-  	end
+	namespace 'api' do
+		namespace 'v1' do
+			  resources :pokemons, only: :index
+			  resources :players, only: [:index, :create]
+			  resources :trades, only: [:index, :create]
+		end
+	end
   end
-end
